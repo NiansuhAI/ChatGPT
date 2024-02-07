14d5b8352a8df3f19a1da49811114e4b8c07cfa8
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-02-07 09:52:42 +0500

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "Niansuh";
+export const REPO = "ChatGPT";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
@@ -106,7 +106,7 @@ Latex block: $$e=mc^2$$
 export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
-  default: "2021-09",
+  default: "2022-01",
   "gpt-4-turbo-preview": "2023-04",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-0125-preview": "2023-04",
